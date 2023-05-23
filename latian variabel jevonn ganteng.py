082cec414c8ae762b3386ed387f1b17bb1c827da
tambss <134348486+tambsss@users.noreply.github.com>
2023-05-23 08:50:33 +0700

    Add files via upload

---
+++ latian variabel jevonn ganteng.py
@@ -0,0 +1,25 @@
+# Variabel adalah tempat menyimpan data
+
+# menaru / assignment nilai
+a = 10
+x = 5
+total = 10000000
+
+# pemanggilan pertama
+print('Nilai a =',a)
+print('Nilai x=',x)
+print('Nilai total =',total)
+
+# penamaan
+nilai_y = 20
+juta1 = 10000000
+nilaiZ = 18,5
+
+# pemanggilan kedua
+print('Nilai a =',a)
+a = 20
+print ('Nilai a=',a)
+
+# assigment indirect 
+b  = a
+print('Nilai a =',a)
